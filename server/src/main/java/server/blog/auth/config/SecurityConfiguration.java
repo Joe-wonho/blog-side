d9ca2d3c58a21bfd0d0a5e52b4514f8dd17f3ce1
HJKKIM <hoj3646@naver.com>
2023-06-09 14:34:38 +0900

    refactor: SecurityConfiguration 설정 추가

--- server/src/main/java/server/blog/auth/config/SecurityConfiguration.java
+++ server/src/main/java/server/blog/auth/config/SecurityConfiguration.java
@@ -6,18 +6,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import server.blog.auth.utils.UserAuthorityUtils;
 
 @Configuration
 public class SecurityConfiguration {
+    private final UserAuthorityUtils authorityUtils;
+
+    public SecurityConfiguration(UserAuthorityUtils authorityUtils) {
+        this.authorityUtils = authorityUtils;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .headers().frameOptions().sameOrigin()
+                .and()
                 .csrf().disable() // CSRF 비활성화
                 .formLogin().disable() // 폼 기반 로그인 비활성화
                 .httpBasic().disable() // HTTP Basic 인증 비활성화
                 .authorizeHttpRequests(authorize -> authorize
-                        .antMatchers("/user/**").hasRole("USER") //"/user/**" 패턴에 대한 요청은 "USER" 권한 사용자만 허용
-                        .antMatchers("/**").permitAll()); // 그 외 모든 요청: 모든 사용자에게 허용
+                        .anyRequest().permitAll()); // 모든 사용자에게 접근 권한 부여
         return http.build();
     }
 
