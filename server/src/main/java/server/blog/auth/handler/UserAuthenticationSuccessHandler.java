239caebeff5c1202588c157434cd4f307bcf05ae
HJKKIM <hoj3646@naver.com>
2023-06-10 01:06:26 +0900

    feat: 로그인 응답으로 AccessToken 리턴 구현

--- server/src/main/java/server/blog/auth/handler/UserAuthenticationSuccessHandler.java
+++ server/src/main/java/server/blog/auth/handler/UserAuthenticationSuccessHandler.java
@@ -1,6 +1,11 @@
 package server.blog.auth.handler;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import server.blog.auth.filter.JwtAuthenticationFilter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -8,6 +13,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 // 로그인 인증 성공 시 로그 기록
 @Slf4j
@@ -16,6 +23,21 @@ public class UserAuthenticationSuccessHandler implements AuthenticationSuccessHa
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException{
-        log.info("Authenticated Successfully");
+        String email = authentication.getName();
+        String accessToken = response.getHeader("Authorization");
+
+        System.out.println("User " + email + " successfully authenticated");
+        try(PrintWriter writer = response.getWriter()){
+            JsonObject json = new JsonObject();
+            json.addProperty("email", email);
+            json.addProperty("AccessToken", accessToken);
+
+            response.setStatus(HttpStatus.ACCEPTED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+
+            writer.write(json.toString());
+        }
+        log.info("# Authenticated successfully!");
     }
 }
