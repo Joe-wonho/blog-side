f457a09d9bbf88c891d246976a48319811fd6972
HJKKIM <hoj3646@naver.com>
2023-06-09 15:48:41 +0900

    feat: 로그인 요청 처리 구현

---
+++ server/src/main/java/server/blog/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,87 @@
+package server.blog.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import server.blog.auth.dto.LoginDto;
+import server.blog.auth.jwt.JwtTokenizer;
+import server.blog.user.entity.User;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+// 사용자 인증 처리, 인증 성공 -> JWT 생성하여 응답 헤더에 추가
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager; // 인증 처리
+    private final JwtTokenizer jwtTokenizer; // JWT 생성, 토큰 관련 도구 제공
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtTokenizer jwtTokenizer) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenizer = jwtTokenizer;
+    }
+
+    // 인증 시도
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    // 인증 성공(JWT 토근 생성, 응답 헤더에 토큰 추가)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) {
+        User user = (User) authResult.getPrincipal();
+
+        String accessToken = delegateAccessToken(user);
+        String refreshToken = delegateRefreshToken(user);
+
+        response.setHeader("Authorization", "Bearer " + accessToken);
+        response.setHeader("Refresh", refreshToken);
+    }
+
+    // AccessToken 생성
+    private String delegateAccessToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", user.getEmail());
+        claims.put("roles", user.getRoles());
+
+        String subject = user.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodedBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+
+    // RefreshToken 생성
+    private String delegateRefreshToken(User user) {
+        String subject = user.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodedBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
+}
