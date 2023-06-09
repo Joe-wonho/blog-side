d7378e5ad6085ae7f8a2ffea9a2869ea6f387ad5
HJKKIM <hoj3646@naver.com>
2023-06-09 14:24:14 +0900

    feat: SecurityConfiguration 설정(접근 권한)

--- server/src/main/java/server/blog/auth/config/SecurityConfiguration.java
+++ server/src/main/java/server/blog/auth/config/SecurityConfiguration.java
@@ -1,5 +1,23 @@
-package server.blog.auth;
+package server.blog.auth.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // CSRF 비활성화
+                .formLogin().disable() // 폼 기반 로그인 비활성화
+                .httpBasic().disable() // HTTP Basic 인증 비활성화
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers("/user/**").hasRole("USER") //"/user/**" 패턴에 대한 요청은 "USER" 권한 사용자만 허용
+                        .antMatchers("/**").permitAll()); // 그 외 모든 요청: 모든 사용자에게 허용
+        return http.build();
+
+    }
 
 }
