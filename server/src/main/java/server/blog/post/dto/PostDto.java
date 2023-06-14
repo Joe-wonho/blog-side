36bfe89bcc617de0d9cfcd50e267aca0fee97d30
HJKKIM <hoj3646@naver.com>
2023-06-14 14:16:31 +0900

    feat: PostDto 생성

---
+++ server/src/main/java/server/blog/post/dto/PostDto.java
@@ -0,0 +1,38 @@
+package server.blog.post.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+public class PostDto {
+    @Getter
+    @Setter
+    public static class Post{
+        private String content;
+        private List<String> img;
+        private List<String> tags;
+//    private String series;
+    }
+
+    @Getter
+    @Setter
+    public static class Patch{
+        private String content;
+        private List<String> img;
+    }
+
+    @Getter
+    @Setter
+    public static class Response{
+        private Long userId;
+        private Long postId;
+        private String content;
+        private List<String> img;
+        private List<String> tag;
+//        private String series;
+        private LocalDateTime createdAt;
+    }
+}
