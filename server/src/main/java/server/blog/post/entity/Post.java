e641620348d758d28ef09888c356872d635454a0
HJKKIM <hoj3646@naver.com>
2023-06-14 14:02:05 +0900

    feat: Post entity 생성

---
+++ server/src/main/java/server/blog/post/entity/Post.java
@@ -0,0 +1,43 @@
+package server.blog.post.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import server.blog.audit.Auditable;
+import server.blog.user.entity.Users;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column
+    private List<String> img;
+    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private List<Post_Tag> postTags = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private Users users;
+
+//    @ManyToOne
+//    @JoinColumn(name = "SERIES_ID")
+//    private String series;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
