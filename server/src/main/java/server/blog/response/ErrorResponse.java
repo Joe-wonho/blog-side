28e80fe5a7a7d19e47089be19323b5d0258a3abf
HJKKIM <hoj3646@naver.com>
2023-06-16 01:31:02 +0900

    refactor: 회원등록 유효성 검사 실패시 응답메세지 에러 메세지 수정

--- server/src/main/java/server/blog/response/ErrorResponse.java
+++ server/src/main/java/server/blog/response/ErrorResponse.java
@@ -6,6 +6,7 @@ import org.springframework.validation.BindingResult;
 import server.blog.exception.ExceptionCode;
 
 import javax.validation.ConstraintViolation;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -14,26 +15,28 @@ import java.util.stream.Collectors;
 public class ErrorResponse {
     private int status;
     private String message;
-    private List<FieldError> fieldErrors;
-    private List<ConstraintViolationError> violationErrors;
+    private List<ErrorField> errors;
 
     private ErrorResponse(int status, String message) {
         this.status = status;
         this.message = message;
     }
 
-    private ErrorResponse(final List<FieldError> fieldErrors,
-                          final List<ConstraintViolationError> violationErrors) {
-        this.fieldErrors = fieldErrors;
-        this.violationErrors = violationErrors;
+    private ErrorResponse(final List<ErrorField> errors) {
+        this.errors = errors;
+    }
+    private ErrorResponse(int status, String message, List<ErrorField> errors) {
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
     }
 
     public static ErrorResponse of(BindingResult bindingResult) {
-        return new ErrorResponse(FieldError.of(bindingResult), null);
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "유효성 검사 실패", ErrorField.of(bindingResult));
     }
 
     public static ErrorResponse of(Set<ConstraintViolation<?>> violations) {
-        return new ErrorResponse(null, ConstraintViolationError.of(violations));
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "유효성 검사 실패", ConstraintViolationError.of(violations));
     }
 
     public static ErrorResponse of(ExceptionCode exceptionCode) {
@@ -48,26 +51,29 @@ public class ErrorResponse {
         return new ErrorResponse(httpStatus.value(), message);
     }
 
-    @Getter
-    public static class FieldError {
-        private String field;
-        private Object rejectedValue;
-        private String reason;
+    public void addError(ErrorField error) {
+        if (this.errors == null) {
+            this.errors = new ArrayList<>();
+        }
+        this.errors.add(error);
+    }
 
-        private FieldError(String field, Object rejectedValue, String reason) {
+    @Getter
+    public static class ErrorField {
+        private String field;
+        private String message;
+
+        private ErrorField(String field, String message) {
             this.field = field;
-            this.rejectedValue = rejectedValue;
-            this.reason = reason;
+            this.message = message;
         }
 
-        public static List<FieldError> of(BindingResult bindingResult) {
+        public static List<ErrorField> of(BindingResult bindingResult) {
             final List<org.springframework.validation.FieldError> fieldErrors =
                     bindingResult.getFieldErrors();
             return fieldErrors.stream()
-                    .map(error -> new FieldError(
+                    .map(error -> new ErrorField(
                             error.getField(),
-                            error.getRejectedValue() == null ?
-                                    "" : error.getRejectedValue().toString(),
                             error.getDefaultMessage()))
                     .collect(Collectors.toList());
         }
@@ -76,22 +82,18 @@ public class ErrorResponse {
     @Getter
     public static class ConstraintViolationError {
         private String propertyPath;
-        private Object rejectedValue;
-        private String reason;
+        private String message;
 
-        private ConstraintViolationError(String propertyPath, Object rejectedValue,
-                                         String reason) {
+        private ConstraintViolationError(String propertyPath, String message) {
             this.propertyPath = propertyPath;
-            this.rejectedValue = rejectedValue;
-            this.reason = reason;
+            this.message = message;
         }
 
-        public static List<ConstraintViolationError> of(
+        public static List<ErrorField> of(
                 Set<ConstraintViolation<?>> constraintViolations) {
             return constraintViolations.stream()
-                    .map(constraintViolation -> new ConstraintViolationError(
+                    .map(constraintViolation -> new ErrorField(
                             constraintViolation.getPropertyPath().toString(),
-                            constraintViolation.getInvalidValue().toString(),
                             constraintViolation.getMessage()
                     )).collect(Collectors.toList());
         }
