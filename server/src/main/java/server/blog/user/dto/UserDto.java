23cc9a7d39334b28c5152d1dad162dab3003bebe
HJKKIM <hoj3646@naver.com>
2023-06-16 13:57:53 +0900

    refactor: dto 유효성 검사 삭제

--- server/src/main/java/server/blog/user/dto/UserDto.java
+++ server/src/main/java/server/blog/user/dto/UserDto.java
@@ -16,25 +16,14 @@ public class UserDto {
     @Getter
     @AllArgsConstructor
     public static class Post {
-        @Valid
-        @NotBlank(message = "이름을 입력하세요.")
         private String name;
 
-        @Valid
-        @NotBlank(message = "닉네임을 입력하세요.")
         private String nickname;
 
-        @Valid
-        @Email(message = "올바른 이메일 형식이 아닙니다.")
-        @NotBlank(message = "이메일을 입력하세요.")
         private String email;
 
-        @Valid
-        @NotBlank(message = "패스워드를 입력하세요.")
-        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()])[a-zA-Z\\d!@#$%^&*()]{8,}$", message = "비밀번호는 최소 8자리이며, 영문, 숫자, 특수문자를 포함해야 합니다.")
         private String password;
 
-        private String profile;
 
     }
 
