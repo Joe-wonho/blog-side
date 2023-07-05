97b6916e91b9e901a3f11f25234be825849095be
HJKKIM <hoj3646@naver.com>
2023-07-05 09:58:51 +0900

    feat: count 값 누적 수정

--- server/src/main/java/server/blog/series/mapper/SeriesMapper.java
+++ server/src/main/java/server/blog/series/mapper/SeriesMapper.java
@@ -6,29 +6,32 @@ import server.blog.series.dto.SeriesDto;
 import server.blog.series.entity.Series;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Mapper(componentModel = "spring")
 public interface SeriesMapper {
 
     default List<SeriesDto.allResponse> seriesToSeriesResponseDto(List<Series> seriesList){
-        List<SeriesDto.allResponse> responseList = new ArrayList<>();
+        Map<String, SeriesDto.allResponse> seriesMap = new HashMap<>();
 
         for (Series series : seriesList) {
-            SeriesDto.allResponse newResponse = new SeriesDto.allResponse();
-            newResponse.setSeriesName(series.getSeriesName());
+            String seriesName = series.getSeriesName();
+            SeriesDto.allResponse response = seriesMap.getOrDefault(seriesName, new SeriesDto.allResponse());
+            response.setSeriesName(seriesName);
 
             List<Post> posts = series.getPosts();
             if (!posts.isEmpty()) {
-                newResponse.setBanner(posts.get(0).getThumbnail());
+                response.setBanner(posts.get(0).getThumbnail());
             }
 
-            newResponse.setCount((long) posts.size());
+            response.setCount((response.getCount() != null ? response.getCount() : 0) + posts.size());
 
-            responseList.add(newResponse);
+            seriesMap.put(seriesName, response);
         }
 
-        return responseList;
+        return new ArrayList<>(seriesMap.values());
     }
 
 }
