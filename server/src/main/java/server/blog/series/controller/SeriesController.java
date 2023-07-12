9b9b547b2e5442e819ff2b15333ce3b94737b498
HJKKIM <hoj3646@naver.com>
2023-07-12 15:05:41 +0900

    refactor: 포스트 시리즈 상세 조회-> 디코딩으로 변환

--- server/src/main/java/server/blog/series/controller/SeriesController.java
+++ server/src/main/java/server/blog/series/controller/SeriesController.java
@@ -25,10 +25,10 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 @RestController
 @RequestMapping
@@ -70,64 +70,70 @@ public class SeriesController {
     // 포스트 시리즈 상세 조회
     @GetMapping("/{nickname}/series/{seriesName}")
     public ResponseEntity<Map<String, Object>> detailSeries(@PathVariable("nickname") String nickname,
-                                                            @PathVariable("seriesName") String seriesName,
+                                                            @PathVariable("seriesName") String encodedSeriesName,
                                                             @Positive @RequestParam int page,
                                                             @Positive @RequestParam int size) {
-        Page<Series> pageSeries = service.findNicknameAndSeries(nickname, seriesName, page - 1, size);
-        List<Series> list = pageSeries.getContent();
+        try {
+            String decodedSeriesName = URLDecoder.decode(encodedSeriesName, StandardCharsets.UTF_8.toString());
 
-        List<SeriesDto.detailResponse> responseList = mapper.seriesToSeriesDetailResponseDto(list);
+            Page<Series> pageSeries = service.findNicknameAndSeries(nickname, decodedSeriesName, page - 1, size);
+            List<Series> list = pageSeries.getContent();
 
-        Map<String, Object> responseData = new LinkedHashMap<>();  // LinkedHashMap 사용
+            List<SeriesDto.detailResponse> responseList = mapper.seriesToSeriesDetailResponseDto(list);
 
-        responseData.put("seriesName", seriesName);
-        responseData.put("post", responseList);
+            Map<String, Object> responseData = new LinkedHashMap<>();
+            responseData.put("seriesName", decodedSeriesName);
+            responseData.put("post", responseList);
 
-        return new ResponseEntity<>(responseData, HttpStatus.OK);
+            return new ResponseEntity<>(responseData, HttpStatus.OK);
+        } catch (UnsupportedEncodingException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Collections.singletonMap("error", "오류가 발생했습니다."));
+        }
     }
 
 
-
-    // 시리즈 추가 (시리즈 생성 / 이미 해당 존재하면 기존 시리즈 반환)
-    @PostMapping("/{nickname}/series")
-    public ResponseEntity createSeries(@PathVariable("nickname") String nickname,
-                                       @RequestParam("userId") Long userId,
-                                       @RequestParam("series") String seriesName) {
-        // 필수 필드 누락 검사
-        if (StringUtils.isEmpty(userId) || StringUtils.isEmpty(seriesName)) {
-            return ResponseEntity.badRequest().body("필수 필드를 입력하세요.");
-        }
-
-        // 액세스 토큰을 사용하여 사용자 정보 가져오기
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName(); // 현재 사용자의 이메일
-
-        // 사용자 정보 확인
-        Users currentUser = userRepository.findByEmail(email).orElse(null);
-        if (currentUser == null || !currentUser.getUserId().equals(userId)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("접근 권한이 없습니다.");
-        }
-
-        // 닉네임으로 사용자 정보 가져오기
-        Users user = userService.findUserByNickname(nickname);
-
-        // 시리즈 생성 또는 기존 시리즈 조회
-        Optional<Series> existingSeries = seriesRepository.findByUsersAndSeriesName(user, seriesName);
-        Series series;
-        if (existingSeries.isPresent()) {
-            series = existingSeries.get();
-        } else {
-            series = new Series();
-            series.setSeriesName(seriesName);
-            series.setUsers(user);
-            seriesRepository.save(series);
-        }
-
-        // 시리즈 생성 응답 생성
-        SeriesDto.Response seriesResponse = new SeriesDto.Response(series.getSeriesId(), series.getSeriesName());
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(seriesResponse);
-    }
+//
+//    // 시리즈 추가 (시리즈 생성 / 이미 해당 존재하면 기존 시리즈 반환)
+//    @PostMapping("/{nickname}/series")
+//    public ResponseEntity createSeries(@PathVariable("nickname") String nickname,
+//                                       @RequestParam("userId") Long userId,
+//                                       @RequestParam("series") String seriesName) {
+//        // 필수 필드 누락 검사
+//        if (StringUtils.isEmpty(userId) || StringUtils.isEmpty(seriesName)) {
+//            return ResponseEntity.badRequest().body("필수 필드를 입력하세요.");
+//        }
+//
+//        // 액세스 토큰을 사용하여 사용자 정보 가져오기
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        String email = authentication.getName(); // 현재 사용자의 이메일
+//
+//        // 사용자 정보 확인
+//        Users currentUser = userRepository.findByEmail(email).orElse(null);
+//        if (currentUser == null || !currentUser.getUserId().equals(userId)) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("접근 권한이 없습니다.");
+//        }
+//
+//        // 닉네임으로 사용자 정보 가져오기
+//        Users user = userService.findUserByNickname(nickname);
+//
+//        // 시리즈 생성 또는 기존 시리즈 조회
+//        Optional<Series> existingSeries = seriesRepository.findByUsersAndSeriesName(user, seriesName);
+//        Series series;
+//        if (existingSeries.isPresent()) {
+//            series = existingSeries.get();
+//        } else {
+//            series = new Series();
+//            series.setSeriesName(seriesName);
+//            series.setUsers(user);
+//            seriesRepository.save(series);
+//        }
+//
+//        // 시리즈 생성 응답 생성
+//        SeriesDto.Response seriesResponse = new SeriesDto.Response(series.getSeriesId(), series.getSeriesName());
+//
+//        return ResponseEntity.status(HttpStatus.CREATED).body(seriesResponse);
+//    }
 
 
 
