cc07136ee51c9c7c7328b63e73da4a77da1a2898
HJKKIM <hoj3646@naver.com>
2023-07-25 16:25:30 +0900

    refactor: ec2 서버 주소 변경

--- server/src/main/java/server/blog/auth/config/SecurityConfiguration.java
+++ server/src/main/java/server/blog/auth/config/SecurityConfiguration.java
@@ -76,7 +76,7 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://ec2-3-39-222-90.ap-northeast-2.compute.amazonaws.com:8080", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://ec2-43-201-50-129.ap-northeast-2.compute.amazonaws.com:8080", "http://localhost:3000"));
 //        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH","DELETE")); // 해당 HTTP 메서드에 대한 통신 허용
 
 //        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
--- server/src/main/java/server/blog/auth/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/server/blog/auth/filter/JwtAuthenticationFilter.java
@@ -77,7 +77,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         cookie1.setHttpOnly(true);
         cookie1.setPath("/");
         cookie1.setMaxAge(3600);
-        cookie1.setDomain("ec2-3-39-222-90.ap-northeast-2.compute.amazonaws.com");
+        cookie1.setDomain("ec2-43-201-50-129.ap-northeast-2.compute.amazonaws.com");
 //        cookie1.setDomain("localhost");
         response.addCookie(cookie1);
 
